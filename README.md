69ecddd566517d51724d901f807a633d3a88407c
Kevin <71783685+kbrault@users.noreply.github.com>
2022-01-24 19:08:55 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # pyWordleCheat
- 
+A simple Wordle solver with no real intelligence nor complexity optimization
+
+# TODO : 
+* Guest words based on letters appearance percentage
+* Optimize complexity
+* Colorization of the script to make it more user friendly
+
+# Usage :
+```shell
+Launch ./python main.go
+```
+
+```shell
+./python main.py
+--------------------------------------------------------------------------------------
+--                                  Wordle Solver                                   --
+--------------------------------------------------------------------------------------
+There is  5757 five letters words in my dictionary (words.txt)
+Hint the first word (how about "ADIEU" to find a maximum of vowels ?) :
+Your hint  1 /5 :
+adieu
+Your hint is : adieu
+What is the result (example : "__X_O" where "_" doesn't exist, "X" exist, "O" is correct) :
+_____
+486 possible words.
+['story', 'known', 'short', 'shown', 'front', 'shows', 'color', 'forms', 'floor', 'books', 'north', 'looks', 'wrong', 'rocks', 'works', 'songs', 'knows', 'brown', 'forth', 'month', 'cross', 'grown', 'crops', 'roots', 'shook', 'cloth', 'worth', 'towns', 'tools', 'block', 'clock', 'grows', 'storm', 'stock', 'sorry', 'throw', 'costs', 'honor', 'worry' [...]
+Your hint  2 /5 :
+howls
+Your hint is : howls
+What is the result (example : "__X_O" where "_" doesn't exist, "X" exist, "O" is correct) :
+_O__O
+76 possible words.
+['forms', 'books', 'rocks', 'songs', 'roots', 'costs', 'rooms', 'boots', 'sorts', 'posts', 'ports', 'socks', 'roofs', 'moons', 'bombs', 'cooks', 'fonts', 'tombs', 'forts', 'corps', 'forks', 'combs', 'tongs', 'cocks', 'booms', 'monks', 'moors', 'mocks', 'toots', 'zooms', 'morns', 'nooks', 'corks', 'corns', 'coops', 'bongs', 'cocos', 'rooks', 'romps', 'noons', 'soots', 'goons', 'norms', 'coons', 'gongs', 'coots', 'kooks', 'boors', 'bozos', 'jocks', 'toons', 'torts', 'comps', 'goofs', 'yoyos', 'boobs', 'boons', 'conks', 'toffs', 'morts', 'corms', 'foots', 'bocks', 'softs', 'pocks', 'poops', 'boffs', 'mosts', 'porks', 'bonks', 'zonks', 'pomps', 'topos', 'moots', 'zooks', 'gooks']
+Your hint  3 /5 :
+jocks 
+Your hint is : jocks
+What is the result (example : "__X_O" where "_" doesn't exist, "X" exist, "O" is correct) :
+_O_OO
+11 possible words.
+['books', 'forks', 'monks', 'nooks', 'rooks', 'kooks', 'porks', 'bonks', 'zonks', 'zooks', 'gooks']
+Your hint  4 /5 :
+porks
+Your hint is : porks
+What is the result (example : "__X_O" where "_" doesn't exist, "X" exist, "O" is correct) :
+_O_OO
+8 possible words.
+['books', 'monks', 'nooks', 'kooks', 'bonks', 'zonks', 'zooks', 'gooks']
+Your hint  5 /5 :
+monks
+Your hint is : monks
+What is the result (example : "__X_O" where "_" doesn't exist, "X" exist, "O" is correct) :
+OOOOO
+1 possible words.
+['monks']
+```
--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-def numberOfWords():
-    return sum(1 for _ in open('words.txt'))
-
 def existInDictionary(word):
     file = open("words.txt")
-    return word in file.read()
+    return word.lower() in file.read()
 
-def searchEngine(hintValue, hintNumber, result, validWord = []):
-    return "Search Engine"
+def searchEngine(hintValue, hintHistory, result, validWords):
+    x = 0
+    unvalidWords = []
+    while x<=4 :
+        # If doesn't exist
+        if(result[x]=="_"):
+            j = 0
+            for word in validWords:
+                if hintValue[x] in word: 
+                    unvalidWords.append(word)
+                j += 1
+
+        # If exist but in bad position
+        if(result[x]=="X"):
+            j = 0
+            for word in validWords:
+                if hintValue[x] not in word: 
+                    unvalidWords.append(word)
+                j += 1
+
+        # If good position
+        if(result[x]=="O"):
+            j = 0
+            for word in validWords:
+                if hintValue[x]!=word[x]: 
+                    unvalidWords.append(word)
+                j += 1
+        x += 1
+
+    return [words for words in validWords if words not in unvalidWords]
 
 def main():
     print("--------------------------------------------------------------------------------------")
     print("--                                  Wordle Solver                                   --")
     print("--------------------------------------------------------------------------------------")
-    print("There is ", numberOfWords(), "five letters words in my dictionary (words.txt)")
-    print("Hint the first word (how about \"ADIEU\" or \"OUIJA\" to find a maximum of vowels ?) :")
+    validWords = [word.rstrip("\n") for word in open('./words.txt')]
+    print("There is ", len(validWords), "five letters words in my dictionary (words.txt)")
+    print("Hint the first word (how about \"ADIEU\" to find a maximum of vowels ?) :")
     i = 1
     while i <= 5:
-        print("Your hint number ", i, " : ")
+        print("Your hint ", i, "/5 : ")
         hint = input()
         # Is this a 5 letter word ? 
         if type(hint) == str and len(hint)==5:
@@ -26,10 +52,13 @@ def main():
                 print("What is the result (example : \"__X_O\" where \"_\" doesn't exist, \"X\" exist, \"O\" is correct) :")
                 result = input()
                 if type(result) == str and len(result)==5:
-                    print(searchEngine(hint, i, result))
+                    newValidWords = searchEngine(hint, i, result, validWords)
+                    validWords = newValidWords
+                    print(len(validWords),"possible words.")
+                    print(validWords)
                     i += 1
                 else : print("The result is not well formatted")
-            else : print("The word doesn't exist in my dictionary (words.txt)")
+            else : print("The word",hint,"doesn't exist in my dictionary (words.txt)")
         else : print("That's not a 5 letters word")
 
 if __name__ == '__main__':
